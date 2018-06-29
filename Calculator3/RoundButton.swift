f5ab20e6b22d18507304a386786b6e6c3cbe2e99
Eunju Tak <teunju@gmail.com>
2018-06-29 17:08:18 +0900

    complete calculator.

---
+++ Calculator3/RoundButton.swift
@@ -0,0 +1,28 @@
+//
+//  RoundButton.swift
+//  Calculator3
+//
+//  Created by 탁은주 on 2018. 4. 10..
+//  Copyright © 2018년 TEJ. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class RoundButton: UIButton {
+
+    @IBInspectable var roundButton: Bool = false {
+        didSet{
+            if roundButton {
+                layer.cornerRadius = frame.height / 2
+            }
+        }
+    }
+    override func prepareForInterfaceBuilder() {
+        if roundButton{
+            layer.cornerRadius = frame.height / 2
+        }
+    }
+
+
+}
--- Calculator3/ViewController.swift
+++ Calculator3/ViewController.swift
@@ -8,18 +8,102 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Operation:String{
+    case Add = "+"
+    case Subtract = "-"
+    case Divide = "/"
+    case Multiply = "*"
+    case NULL = "Null"
+    
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var runningNumber = ""
+    var leftValue = ""
+    var rightValue = ""
+    var result = ""
+    var currentOperation:Operation = .NULL
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        outputLabel.text = "0"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressedAction(_ sender: RoundButton) {
+        if runningNumber.count <= 8{
+            runningNumber += "\(sender.tag)"
+            outputLabel.text = runningNumber
+        }
     }
-
-
+    @IBAction func allClearPressedAction(_ sender: RoundButton) {
+        runningNumber = ""
+        leftValue = ""
+        rightValue = ""
+        result = ""
+        currentOperation = .NULL
+        outputLabel.text = "0"
+    }
+    @IBAction func dotPressedAction(_ sender: RoundButton) {
+        if runningNumber.count <= 7{
+            runningNumber += "."
+            outputLabel.text = runningNumber
+        }
+    }
+    @IBAction func dividePressedAction(_ sender: RoundButton) {
+        operation(operation: .Divide)
+    }
+    @IBAction func multiplyPressedAction(_ sender: RoundButton) {
+        if outputLabel.text == rightValue {
+            outputLabel.text = result
+        }
+        operation(operation: .Multiply)
+    }
+    @IBAction func subtractPressedAction(_ sender: RoundButton) {
+        operation(operation: .Subtract)
+    }
+    @IBAction func addPressedAction(_ sender: RoundButton) {
+        operation(operation: .Add)
+    }
+    @IBAction func equalPressedAction(_ sender: RoundButton) {
+        operation(operation: currentOperation)
+    }
+    
+    
+    
+    func operation(operation:Operation){
+        if currentOperation != .NULL{
+            if runningNumber != ""{
+                rightValue = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == .Add{
+                    result = "\(Double(leftValue)! + Double(rightValue)!)"
+                }else if currentOperation == .Subtract{
+                    result = "\(Double(leftValue)! - Double(rightValue)!)"
+                    
+                }else if currentOperation == .Divide{
+                    result = "\(Double(leftValue)! / Double(rightValue)!)"
+                    
+                }else if currentOperation == .Multiply{
+                    result = "\(Double(leftValue)! * Double(rightValue)!)"
+                }
+                leftValue = result
+                if (Double(result)!.truncatingRemainder(dividingBy: 1) == 0){
+                    result = "\(Int(Double(result)!))"
+                }
+                outputLabel.text = result
+            }
+            currentOperation = operation
+            
+        }else {
+            leftValue = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
 }
+
 
